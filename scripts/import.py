6d0cb2899e8344b0214c0353b2f35da1a1613620
r888888888 <r888888888@gmail.com>
2018-07-21 22:56:24 -0700

    switch terminology to favs

--- scripts/import.py
+++ scripts/import.py
@@ -14,7 +14,7 @@ import datetime
 load_dotenv(find_dotenv())
 
 CONFIDENCE = 5
-MIN_VOTES = 10
+MIN_FAVS = 10
 GBQ_PROJECT_ID = "danbooru-1343"
 GBQ_TABLE = "danbooru_production.favorites"
 GBQ_KEY_PATH = os.environ.get("GOOGLE_JSON_KEY")
@@ -23,6 +23,15 @@ ALS_FACTORS = 128
 ALS_REGULARIZATION = 1e-2
 ALS_ITERATIONS = 15
 
+def query_csv(year, month):
+  path = "/tmp/favorites-{year}-{month:02d}-01.csv".format(year=year, month=month)
+  data = pd.read_csv(path, header=None, names=["post_id", "user_id"])
+  try:
+    os.makedirs(MATRIX_PATH + "/favs/{year}".format(year=year))
+  except os.error:
+    None
+  data.to_pickle(MATRIX_PATH + "/favs/{year}/{month}.pickle".format(year=year, month=month))
+
 def query_gbq(year, month):
   start = '{year}-{month:02d}-01 00:00:00'.format(year=year, month=month)
   stop = "{year}-{month:02d}-{eod} 00:00:00".format(year=year, month=month, eod=calendar.monthrange(year, month)[1])
@@ -30,10 +39,10 @@ def query_gbq(year, month):
   query = 'SELECT post_id, user_id FROM [{project_id}:{table}] WHERE _PARTITIONTIME >= "{start}" AND _PARTITIONTIME < "{stop}" LIMIT {limit}'.format(project_id=GBQ_PROJECT_ID, table=GBQ_TABLE, start=start, stop=stop, limit=limit)
   data = pd.read_gbq(query, project_id=GBQ_PROJECT_ID, private_key=GBQ_KEY_PATH)
   try:
-    os.makedirs(MATRIX_PATH + "/votes/{year}".format(year=year))
+    os.makedirs(MATRIX_PATH + "/favs/{year}".format(year=year))
   except os.error:
     None
-  data.to_pickle(MATRIX_PATH + "/votes/{year}/{month}.pickle".format(year=year, month=month))
+  data.to_pickle(MATRIX_PATH + "/favs/{year}/{month}.pickle".format(year=year, month=month))
 
 def seed_gbq():
   for month in range(1, 8):
@@ -48,22 +57,23 @@ def train_model():
     regularization=ALS_REGULARIZATION,
     iterations=ALS_ITERATIONS
   )
-  data = pd.concat([pd.read_pickle(f) for f in Path(MATRIX_PATH).glob("votes/**/*.pickle")])
+  data = pd.concat([pd.read_pickle(f) for f in Path(MATRIX_PATH).glob("favs/**/*.pickle")])
+  data = pd.concat([data, ])
   print(data.shape)
-  data = data.groupby("user_id").filter(lambda x: x["user_id"].count() >= MIN_VOTES)
+  data = data.groupby("user_id").filter(lambda x: x["user_id"].count() >= MIN_FAVS)
   print(data.shape)
   data["user_id"] = data["user_id"].astype("category")
   data["post_id"] = data["post_id"].astype("category")
-  votes_coo = coo_matrix((np.ones(data.shape[0]), (data["post_id"].cat.codes.copy(), data["user_id"].cat.codes.copy()))) * CONFIDENCE
-  votes_csr = votes_coo.tocsr()
-  model.fit(votes_coo)
+  favs_coo = coo_matrix((np.ones(data.shape[0]), (data["post_id"].cat.codes.copy(), data["user_id"].cat.codes.copy()))) * CONFIDENCE
+  favs_csr = favs_coo.tocsr()
+  model.fit(favs_coo)
   posts_to_id = {k: v for v, k in enumerate(data["post_id"].cat.categories)}
   ids_to_post = {k: v for v, k in posts_to_id.items()}
   with open(MATRIX_PATH + "/i2p.pickle", "wb") as file:
     pickle.dump(ids_to_post, file)
   with open(MATRIX_PATH + "/p2i.pickle", "wb") as file:
     pickle.dump(posts_to_id, file)
-  save_npz(MATRIX_PATH + "/csr", votes_csr)
+  save_npz(MATRIX_PATH + "/csr", favs_csr)
   with open(MATRIX_PATH + "/model.pickle", "wb") as file:
     pickle.dump(model, file)
 
