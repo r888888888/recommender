4eb525570279726c9131928df5cef06bce1e3d62
Albert Yi <albert@carbonfive.com>
2018-09-13 14:18:05 -0700

    fix recommendation endpoint

--- web/recommender.py
+++ web/recommender.py
@@ -50,7 +50,7 @@ def recommend(user_id):
   if not user_id in _users_to_id:
     return jsonify(error="user not in database"), 404
   uid = _users_to_id[user_id]
-  matches = _model.recommend(uid, _csr.getrow(uid), N=30)
+  matches = _model.recommend(uid, _csr, N=30)
   matches = [(_ids_to_post[idx], str(score)) for idx, score in matches]
   return jsonify(matches)
 
