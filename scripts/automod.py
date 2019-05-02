608cecd8dfbebc1c30afccc5c300e0f46e40b1fd
Albert Yi <albert@carbonfive.com>
2019-05-01 18:09:18 -0700

    add randomizedsearch

--- scripts/automod.py
+++ scripts/automod.py
@@ -5,6 +5,7 @@ import pprint
 from sklearn.model_selection import train_test_split, cross_val_score
 import pickle
 import os
+from sklearn.model_selection import RandomizedSearchCV
 
 fields = {
   'artist_count': lambda x: get_number(x['artist_count']),
@@ -16,7 +17,7 @@ fields = {
   'file_size': lambda x: get_number(x['file_size']),
   'width': lambda x: get_number(x['width']),
   'height': lambda x: get_number(x['height']),
-  'median_score': lambda x: float(get_number(x.get('median_score', {'N': 0}))),
+  'uploader_median_score': lambda x: float(get_number(x.get('median_score', {'N': 0}))),
   'artist_identified': lambda x: int(x['artist_identified']['BOOL']),
   'copyright_identified': lambda x: int(x['copyright_identified']['BOOL']),
   'character_identified': lambda x: int(x['character_identified']['BOOL']),
@@ -30,6 +31,7 @@ client = boto3.client('dynamodb', region_name="us-west-1")
 X = None
 y = None
 pp = pprint.PrettyPrinter()
+classifier = None
 
 def get_number(obj):
   if "N" in obj:
@@ -43,54 +45,74 @@ def extractFeatures(items):
 def extractResults(items):
   return np.array([int(x['is_approved']['BOOL']) for x in items]).astype(int)
 
-if os.path.isfile("X.bin"):
-  with open("X.bin", "rb") as file:
-    X = pickle.load(file)
+if os.path.isfile("model.pickle"):
+  print("Loading pickled model")
+  with open("model.pickle", "rb") as file:
+    classifier = pickle.load(file)
 
-if os.path.isfile("y.bin"):
-  with open("y.bin", "rb") as file:
-    y = pickle.load(file)
+if classifier is None:
+  if os.path.isfile("X.bin"):
+    with open("X.bin", "rb") as file:
+      X = pickle.load(file)
 
-has_more = True
-limit = 4000
-last_evaluated_key = None
-if X is None and y is None:
-  X = np.zeros(shape=(0, len(fields.keys())))
-  y = np.array([])
-  while has_more:
-    if last_evaluated_key:
-      results = client.scan(TableName="automod_events_production", Limit=limit, ExclusiveStartKey=last_evaluated_key)
-    else:
-      results = client.scan(TableName="automod_events_production", Limit=limit)
+  if os.path.isfile("y.bin"):
+    with open("y.bin", "rb") as file:
+      y = pickle.load(file)
+
+  if X is None and y is None:
+    has_more = True
+    last_evaluated_key = None
+    X = np.zeros(shape=(0, len(fields.keys())))
+    y = np.array([])
+    while has_more:
+      if last_evaluated_key:
+        results = client.scan(TableName="automod_events_production", ExclusiveStartKey=last_evaluated_key)
+      else:
+        results = client.scan(TableName="automod_events_production")
       if "LastEvaluatedKey" in results:
         last_evaluated_key = results['LastEvaluatedKey']
       else:
         has_more = False
-    features = extractFeatures(results['Items'])
-    results = extractResults(results['Items'])
-    print(X.shape)
-    X = np.concatenate((X, features))
-    y = np.concatenate((y, results))
-    if X.shape[0] > 1000000:
-      has_more = False
-  with open("X.bin", "wb") as file:
-    pickle.dump(X, file)
-  with open("y.bin", "wb") as file:
-    pickle.dump(y, file)
+      features = extractFeatures(results['Items'])
+      results = extractResults(results['Items'])
+      print("Querying", X.shape)
+      X = np.concatenate((X, features))
+      y = np.concatenate((y, results))
+      if X.shape[0] > 1000000:
+        has_more = False
+    with open("X.bin", "wb") as file:
+      pickle.dump(X, file)
+    with open("y.bin", "wb") as file:
+      pickle.dump(y, file)
 
-classifier = RandomForestClassifier(n_estimators=100)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-importances = {}
-for i, key in enumerate(fields.keys()):
-  importances[key] = classifier.feature_importances_[i]
+  classifier = RandomForestClassifier()
+  # X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-pp.pprint(sorted(importances.items(), key=lambda x: x[1]))
+  param_grid = { 
+    n_estimators: [10, 100, 150],
+    bootstrap: [True, False],
+    max_depth: [None, 10, 20, 30, 40, 50],
+    min_samples_split: [2, 5, 10],
+    min_samples_leaf: [1, 2, 4]
+  }
 
-from sklearn.metrics import classification_report
+  random_search = RandomizedSearchCV(estimator=classifier, param_distributions=param_grid, n_iter=100, cv=3, verbose=2, n_jobs=-1)
+  random_search.fit(X, y)
+  print(random_search.best_params_)
 
-print(classification_report(y_test, y_pred))
+  with open("model.pickle", "wb") as file:
+    pickle.dump(classifier, file)
 
-scores = cross_val_score(classifier, X_test, y_test, cv=5)
-print(scores)
+# y_pred = classifier.predict(X_test)
+# importances = {}
+# for i, key in enumerate(fields.keys()):
+#   importances[key] = classifier.feature_importances_[i]
+
+# pp.pprint(sorted(importances.items(), key=lambda x: x[1]))
+
+# from sklearn.metrics import classification_report
+
+# print(classification_report(y_test, y_pred))
+
+# scores = cross_val_score(classifier, X_test, y_test, cv=5)
+# print(scores)
